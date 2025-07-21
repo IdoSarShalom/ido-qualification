69c451f7d0f09635caf74286d0d02ffb37177d29
Ido Sar Shalom <idosarshalom@gmail.com>
2025-07-21 11:36:53 +0300

    refactor: use unique pointer. no need to deallocate manually the smart pointer handles it.

--- src/level1/basics5/integer_dynamic_allocate.cpp
+++ src/level1/basics5/integer_dynamic_allocate.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 int get_integer() {
     int user_input;
@@ -7,27 +8,22 @@ int get_integer() {
     return user_input;
 }
 
-int* allocate_dynamic_integer(const int integer_value) {
-    const auto dynamic_integer = new int;
-    *dynamic_integer = integer_value;
+std::unique_ptr<int> allocate_dynamic_integer(const int integer_value) {
+    auto dynamic_integer = std::make_unique<int>(integer_value);
+
     return dynamic_integer;
 }
 
-void print_integer(const int* dynamic_integer, const int original_value) {
+void print_integer(const std::unique_ptr<int>& dynamic_integer, const int original_value) {
     std::cout << "\n--- Value Output ---" << std::endl;
     std::cout << "Value accessed via pointer: " << *dynamic_integer << std::endl;
     std::cout << "Original input value: " << original_value << std::endl;
 }
 
-void deallocate_dynamic_integer(const int* dynamic_integer) {
-    delete dynamic_integer;
-    dynamic_integer = nullptr;
-}
-
 int main() {
     const int integer_input = get_integer();
-    const int* dynamic_integer = allocate_dynamic_integer(integer_input);
+    const auto dynamic_integer = allocate_dynamic_integer(integer_input);
     print_integer(dynamic_integer, integer_input);
-    deallocate_dynamic_integer(dynamic_integer);
+
     return 0;
 }
